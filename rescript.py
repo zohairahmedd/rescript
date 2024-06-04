5698e09a85ba72762ef9c93df9b4767e53354336
zohairahmedd <151594538+zohairahmedd@users.noreply.github.com>
2024-06-04 15:26:13 -0400

    added counter variables

--- rescript.py
+++ rescript.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import re
 
-df = pd.read_excel('EBR_Videos_Listed_Excel.xlsx') # dataframe
+df = pd.read_csv('EBR_Videos_Listed.csv') # dataframe
 
 categories = { # maps category names to a list of specific keywords
-    'Unusable': ['Unusable', "can't", 'really', 'bad', 'positioning', 'blurry', 'too', 'hidden', 'Too'],
-    'Can Be Used': ['minute', 'first', 'half', 'little', 'dark', 'harder', 'maybe', 'might'],
+    'Unusable': ['Unusable', 'really', 'bad', 'positioning', 'blurry', 'too', 'hidden', 'Too'],
+    'Can Be Used': ['minute', 'last', 'first', 'half', 'little', 'dark', 'harder', 'maybe', 'might', 'usable', 'few', 'some', 'frames'],
     'Perfect Video': ['Clear', 'visible'],
 } # notice how its in order of unusable to perfect, this is mandatory
 
 pattern = r'[a-zA-Z]+' # matches sequences of entire alphabet
+totalCounter = 0
+unusableCounter = 0
+potentialCounter = 0
+perfectCounter = 0
 
 for index, row in df.iterrows(): # iterates over each row in dataframe or excel sheet
 
@@ -17,7 +21,19 @@ for index, row in df.iterrows(): # iterates over each row in dataframe or excel 
     annotation = row['Brief Description'] # extracts annotations
     
     if re.search(pattern, value): # if text is found in a row within video name, continue
+        totalCounter += 1
         for category, keywords in categories.items(): # iterate through CATEGORIES, keywords temporarily holds the list of keywords associated with the current category
             if any(keyword.lower() in annotation.lower() for keyword in keywords): # compares lowercased keyword to lowercased version of annotated text, loops for all keywords in category
                 print(f"Categorizing '{value}' under '{category}'") # prints only if a match is found
+                if category == 'Unusable':
+                    unusableCounter += 1
+                if category == 'Can Be Used':
+                    potentialCounter += 1
+                if category == 'Perfect Video':
+                    perfectCounter += 1
                 break # break so that each video is only categorized once
+
+print(f"Total Rows: {totalCounter}")
+print(f"Unusable Videos: {unusableCounter}")
+print(f"Potential Videos: {potentialCounter}")
+print(f"Perfect Videos: {perfectCounter}")
